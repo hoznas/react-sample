b5e77ae2650dd912e9d0c3a75efa2501137121b7
hoznas <s_unkun@yahoo.co.jp>
2022-11-03 20:32:36 +0900

    App Hello Name Sample

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//import App from './App';
+//import Hello from './components/Hello'
+//import Name from './components/Name'
+import Message from './components/Message'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Message />
+    {/*<App /><Hello /><Name />*/}
   </React.StrictMode>
 );
 
